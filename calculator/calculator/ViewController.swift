eddd91c0bbd2795f5bf816bdfedc6ba48f1ffeb4
Sydney <chnsydney@gmail.com>
2018-05-24 19:11:20 +0200

    buttons

---
+++ calculator/calculator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by Sydney Cohen on 24/05/2018.
+//  Copyright © 2018 Sydney Cohen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var results: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
