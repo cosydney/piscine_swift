cdc0fbedc9c1998c9eaee0db5334a86cc07eeb2a
Sydney <chnsydney@gmail.com>
2018-05-24 17:37:55 +0200

    ""

--- DOO/DOO/ViewController.swift
+++ DOO/DOO/ViewController.swift
@@ -10,8 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var hellolabel: UILabel!
+    
+    @IBAction func button(_ sender: UIButton) {
+        if hellolabel.text == "Hello World !"{
+            hellolabel.text = "Hello"
+        }
+        else {
+            hellolabel.text = "Hello World !"
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("viewdidLoad")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
