608a8254f39f3e9da6ad1288f4024f5fb1c210e7
Sydney COHEN <chnsydney@gmail.com>
2018-05-28 10:15:15 +0200

    "ex01"

--- D00/ex01/ex01/ViewController.swift
+++ D00/ex01/ex01/ViewController.swift
@@ -13,13 +13,23 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("viewddload")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    @IBOutlet weak var hellolabel: UILabel!
+    @IBAction func clickbutton(_ sender: UIButton) {
+        if hellolabel.text == "Hello World !"{
+            hellolabel.text = "Hello"
+        }
+        else {
+            hellolabel.text = "Hello World !"
+        }
+    }
+    
 
 }
 
