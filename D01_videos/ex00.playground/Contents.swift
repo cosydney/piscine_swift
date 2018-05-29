bf7878709d23355e2fb1ee2745e0e9d1ae51c9ca
Sydney COHEN <chnsydney@gmail.com>
2018-05-29 13:10:56 +0200

    ""

--- D01_videos/ex00.playground/Contents.swift
+++ D01_videos/ex00.playground/Contents.swift
@@ -1,48 +1,141 @@
 //: Playground - noun: a place where people can play
 
+// Color
 enum Color : String {
-    case hearts = "hearts"
-    case diamonds = "diamonds"
-    case clubs = "clubs"
-    case spades = "spades"
-    static let allColor : [Color] = [hearts, diamonds, clubs, spades]
+    case Heart = "Heart"
+    case Diamond = "Diamond"
+    case Club = "Club"
+    case Spade = "Spade"
+    static let allColor : [Color] = [Heart, Diamond, Club, Spade]
 }
 
+//Value
 enum Value : Int {
-    case ace = 1
-    case two, three, four, five, six, seven, eight, nine, ten
-    case jack, queen, king
+    case Ace = 1
+    case Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten
+    case Jack, Queen, King
     
     static let allValue : [Value] = [
-    ace,
-    two,
-    three,
-    four,
-    five,
-    six,
-    seven,
-    eight,
-    nine,
-    ten,
-    jack,
-    queen,
-    king
+        Ace,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King
     ]
 }
 
+//Card
 import Foundation
 
-var arrayValue : [Value] = Value.allValue
-var arrayColor : [Color] = Color.allColor
-
-for value in arrayValue {
-    print (value, "is", value.rawValue)
+class Card: NSObject {
+    var c : Color
+    var v : Value
+    
+    override var description: String {
+        if self.v.rawValue > 10 {
+            return "(\(self.v), \(self.c))"
+        } else {
+            return "(\(self.v.rawValue), \(self.c))"
+        }
+    }
+    
+    init(c: Color, v: Value) {
+        self.c = c
+        self.v = v
+        super.init()
+    }
+    override func isEqual(_ object: Any?) -> Bool {
+        if let obj = object as? Card{
+            return (obj.c == self.c && obj.v == self.v)
+        }
+        return false
+    }
 }
 
-for color in arrayColor {
-    print (color, "is", color.rawValue)
+func ==(cardone: Card, cardtwo: Card) -> Bool
+{
+    return (cardone.c == cardtwo.c && cardone.v == cardtwo.v )
 }
 
+//Deck
+class Deck: NSObject {
+    static let allSpades    : [Card] = Value.allValue.map({Card(c:Color.Spade, v:$0)})
+    static let allHearts    : [Card] = Value.allValue.map({Card(c:Color.Heart, v:$0)})
+    static let allClubs     : [Card] = Value.allValue.map({Card(c:Color.Club, v:$0)})
+    static let allDiamonds  : [Card] = Value.allValue.map({Card(c:Color.Diamond, v:$0)})
+    static let allCards     : [Card] = allSpades + allHearts + allClubs + allDiamonds
+}
+extension Array {
+    mutating func shuffle(){
+        for i in indices {
+            let j = Int(arc4random_uniform(UInt32(self.count - i))) + i
+            if (j != i){
+                self.swapAt(i,j)
+            }
+        }
+    }
+}
+
+// Main ex03
+var shufflecards = Deck.allCards
+print("Taking a brand new deck")
+print(shufflecards)
+print("Shuffling cards")
+shufflecards.shuffle()
+print(shufflecards)
+print("Shuffling cards again")
+shufflecards.shuffle()
+print(shufflecards)
 
 
+//Main ex02
+//print("Total number of cards in the deck:", Deck.allCards.count)
+//print(Deck.allSpades)
+//print(Deck.allHearts)
+//print(Deck.allClubs)
+//print(Deck.allDiamonds)
+//print(Deck.allCards)
+
+////Main ex 00
+//import Foundation
+//
+//var arrayValue : [Value] = Value.allValue
+//var arrayColor : [Color] = Color.allColor
+//
+//for value in arrayValue {
+//    print (value, "is", value.rawValue)
+//}
+//
+//for color in arrayColor {
+//    print (color, "is", color.rawValue)
+//}
+
+////Main ex01
+//let card1 : Card = Card(c: Color.Spade, v: Value.Ace)
+//let card2 : Card = Card(c: Color.Diamond, v: Value.Two)
+//let card3 : Card = Card(c: Color.Spade, v: Value.Ace)
+//let card4 : Card = Card(c: Color.Spade, v: Value.Jack)
+//
+//print ("card1:", card1)
+//print ("card2:", card2)
+//print ("card3:", card3)
+//print ("card4:", card4)
+//
+//print("\n Testing isEqual")
+//print("card1.isEqual(card2) should evaluate to false:", card1.isEqual(card2))
+//print("card1.isEqual(card3) should evaluate to true:", card1.isEqual(card3))
+//print("card2.isEqual(card3) should evaluate to false:", card2.isEqual(card3))
+//
+//print("\n Testing ==")
+//print("card1 == card2, should evaluate to false:", card1 == card2)
+//print("card1 == card3, should evaluate to true:", card1 == card3)
+//print("card2 == card3, should evaluate to false:", card2 == card3)
 
