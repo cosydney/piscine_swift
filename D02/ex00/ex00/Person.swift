014db24ca8858c0d50fb3396dbd02ced1af8b952
Sydney COHEN <chnsydney@gmail.com>
2018-05-30 11:21:29 +0200

    ""

---
+++ D02/ex00/ex00/Person.swift
@@ -0,0 +1,18 @@
+//
+//  Person.swift
+//  ex00
+//
+//  Created by Sydney COHEN on 5/30/18.
+//  Copyright © 2018 Sydney COHEN. All rights reserved.
+//
+
+import Foundation
+
+struct Data {
+    static let peoples : [(String, Int)] = [
+    ("Jesus", 0),
+    ("Mickael Jackson", 2009),
+    ("Albert Einstein", 1955),
+    ("Stephen Hawking", 2018)
+    ]
+}
---
+++ D02/ex00/ex00/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  ex00
+//
+//  Created by Sydney COHEN on 5/30/18.
+//  Copyright © 2018 Sydney COHEN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableview: UITableView! {
+        didSet {
+            tableview.delegate = self
+            tableview.dataSource = self
+        }
+    }
+        
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Data.peoples.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "personCell")
+        cell?.textLabel?.text = Data.peoples[indexPath.row].0
+        cell?.detailTextLabel?.text = String(Data.peoples[indexPath.row].1)
+        return cell!
+    }
+}
+
