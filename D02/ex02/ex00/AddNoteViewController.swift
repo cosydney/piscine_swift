bc603f288b71763efa7237448b99f98b18ae1723
Sydney COHEN <chnsydney@gmail.com>
2018-05-30 14:20:08 +0200

    "ex02"

---
+++ D02/ex02/ex00/AddNoteViewController.swift
@@ -0,0 +1,44 @@
+//
+//  AddNoteViewController.swift
+//  ex00
+//
+//  Created by Sydney COHEN on 5/30/18.
+//  Copyright © 2018 Sydney COHEN. All rights reserved.
+//
+
+import UIKit
+
+class AddNoteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBOutlet weak var namefield: UITextField!
+    @IBOutlet weak var descriptionfield: UITextField!
+    @IBOutlet weak var datefield: UIDatePicker!
+
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        print ("namefield", namefield.text)
+        print ("desc", descriptionfield.text)
+        print ("datefield", datefield.date)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
