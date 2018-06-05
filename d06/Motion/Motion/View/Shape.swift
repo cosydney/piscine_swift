22660d611470951ac6b855738239f6e55fcf4bc7
Sydney COHEN <chnsydney@gmail.com>
2018-06-05 14:35:04 +0200

    "circles"

---
+++ d06/Motion/Motion/View/Shape.swift
@@ -0,0 +1,81 @@
+//
+//  Shape.swift
+//  Motion
+//
+//  Created by Sydney COHEN on 6/5/18.
+//  Copyright © 2018 Sydney COHEN. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Shape: UIView {
+    /*
+     // Only override draw() if you perform custom drawing.
+     // An empty implementation adversely affects performance during animation.
+     override func draw(_ rect: CGRect) {
+     // Drawing code
+     }
+     */
+    var size : CGFloat = 100
+    var isCircle = false
+    init(point: CGPoint, maxwidth: CGFloat, maxheight: CGFloat ) {
+        var x = point.x
+        var y = point.y
+        
+        // form shouldnt be able to go out of scope
+        if x+size/2 > maxwidth {
+            x -= size/2
+        }
+        if y+size/2 > maxheight {
+            y -= size/2
+        }
+        
+        // generate circle or square
+        let random = Int(arc4random_uniform(2))
+        switch random {
+        case 0 :
+            super.init(frame: CGRect(x: x, y: y, width: size, height: size))
+            self.layer.cornerRadius = size/2
+            self.isCircle = true
+        default:
+            super.init(frame: CGRect(x: x, y: y, width: size, height: size))
+        }
+        self.backgroundColor = generateRandomColor()
+    }
+    
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    func generateRandomColor() -> UIColor {
+        let hue : CGFloat = CGFloat(arc4random() % 256) / 256
+        let saturation : CGFloat = CGFloat(arc4random() % 128) / 256 + 0.5
+        let brightness : CGFloat = CGFloat(arc4random() % 128) / 256 + 0.5
+        
+        return UIColor(hue: hue, saturation: saturation, brightness: brightness, alpha: 1)
+    }
+    
+    override var collisionBoundsType: UIDynamicItemCollisionBoundsType {
+        return .path
+    }
+    
+    override var collisionBoundingPath: UIBezierPath {
+        
+        if self.isCircle {
+            let radius = min(bounds.size.width, bounds.size.height) / 2.0
+            return UIBezierPath(arcCenter: CGPoint.zero, radius: radius, startAngle: 0, endAngle: CGFloat(Double.pi * 2.0), clockwise: true)
+        }
+        else {
+            var rect = bounds
+            rect.origin.x -= rect.size.width / 2
+            rect.origin.y -= rect.size.height / 2
+            return UIBezierPath(rect: rect)
+        }
+
+    }
+}
+
+
--- d06/Motion/Motion/ViewController.swift
+++ d06/Motion/Motion/ViewController.swift
@@ -11,58 +11,148 @@ import CoreMotion
 
 class ViewController: UIViewController {
     
-    private var dynamiqueAnimator : UIDynamicAnimator!
-    private let gravityBehavior = UIGravityBehavior()
-    
-    private static let gravityScale = 2.5
-    
-    private let motionManager = CMMotionManager()
+    var animator: UIDynamicAnimator!
+    var gravity: UIGravityBehavior!
+    var collision: UICollisionBehavior!
+    var itemBehaviour: UIDynamicItemBehavior!
+    let motionManager = CMMotionManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Hello World")
-        let gesture = UIPanGestureRecognizer(target: self, action: #selector(self.panGesture(gesture:)))
-        view.addGestureRecognizer(gesture)
+        animator = UIDynamicAnimator(referenceView: view)
+        gravity = UIGravityBehavior(items: [])
+        animator.addBehavior(gravity)
         
-//
-        dynamiqueAnimator = UIDynamicAnimator(referenceView: self.view)
+        collision = UICollisionBehavior(items: [])
+        collision.translatesReferenceBoundsIntoBoundary = true
+        animator.addBehavior(collision)
         
-        dynamiqueAnimator.addBehavior(gravityBehavior)
-        motionManager.accelerometerUpdateInterval = 0.2
-        let queue = OperationQueue()
-        motionManager.startAccelerometerUpdates(to: queue, withHandler: accelerometerHandler as! CMAccelerometerHandler)
+        itemBehaviour = UIDynamicItemBehavior(items: [])
+        itemBehaviour.elasticity = 0.3
+        animator.addBehavior(itemBehaviour)
     }
     
-    func accelerometerHandler(data: CMAccelerometerData?, error : NSError?) {
-        if error == nil && data != nil {            
-            let gravityVector = CGVector(dx: CGFloat(data!.acceleration.x *
-                ViewController.gravityScale),dy: CGFloat(-data!.acceleration.y * ViewController.gravityScale))
-            gravityBehavior.gravityDirection = gravityVector
+    override func viewWillAppear(_ animated: Bool) {
+        print("viewillappear")
+        super.viewWillAppear(animated)
+        if motionManager.isAccelerometerAvailable {
+            motionManager.accelerometerUpdateInterval = 0.2
+            let queue = OperationQueue.main
+            motionManager.startAccelerometerUpdates(to: queue, withHandler: accHandler)
+        }
+        else {
+            print("accelerometer is not available")
+        }
+    }
+    
+    func accHandler(data: CMAccelerometerData?, error: Error?) {
+        if let myData = data {
+            let x = CGFloat(myData.acceleration.x);
+            let y = CGFloat(myData.acceleration.y);
+            let v = CGVector(dx: x, dy: -y);
+            gravity.gravityDirection = v;
         }
     }
     
     @IBAction func tap(_ sender: UITapGestureRecognizer) {
         let gpoint = sender.location(in: self.view)
-        let myView = generateRandomView(gpoint: gpoint)
-        myView.backgroundColor = generateRandomColor()
-
-        let dynamicAnimator = UIDynamicAnimator()
-        let gravityBehavior = UIGravityBehavior()
-        gravityBehavior.magnitude = 1000
-        dynamicAnimator.addBehavior(gravityBehavior)
-        gravityBehavior.addItem(myView)
-        gravityBehavior.removeItem(myView)
+        let myView = Shape(point: gpoint,
+                          maxwidth: self.view.bounds.width,
+                          maxheight: self.view.bounds.height)
+        
         view.addSubview(myView)
+        gravity.addItem(myView)
+        collision.addItem(myView)
+        itemBehaviour.addItem(myView)
+        
+        let gesture = UIPanGestureRecognizer(target: self, action: #selector(self.panGesture(gesture:)))
+        myView.addGestureRecognizer(gesture)
+        
+        let press = UITapGestureRecognizer(target: self, action: #selector(self.handleLongPress(press:)))
+        myView.addGestureRecognizer(press)
+        
+        let pinch = UIPinchGestureRecognizer(target: self, action: #selector(handlePinch(recognizer:)))
+        myView.addGestureRecognizer(pinch)
+        
+        let rotate = UIRotationGestureRecognizer(target: self, action:#selector(self.handleRotate(rotate:)))
+        myView.addGestureRecognizer(rotate)
+    }
+    
+    @objc func handlePinch(recognizer : UIPinchGestureRecognizer) {
+        print("handlePinch")
+        if let view = recognizer.view {
+            print(recognizer.scale)
+            
+            switch recognizer.state {
+            case .began:
+                print("PBegan")
+                self.gravity.removeItem(view)
+                self.collision.removeItem(view)
+                self.itemBehaviour.removeItem(view)
+                
+            case.changed:
+                print("PChanged")
+                recognizer.view?.layer.bounds.size.height *= recognizer.scale
+                recognizer.view?.layer.bounds.size.width *= recognizer.scale
+                if let tmp = recognizer.view! as? Shape {
+                    if (tmp.isCircle) {recognizer.view!.layer.cornerRadius *= recognizer.scale}}
+                recognizer.scale = 1
+                
+            case .ended:
+                print("PEnded")
+                self.gravity.addItem(view)
+                self.collision.addItem(view)
+                self.itemBehaviour.addItem(view)
+                
+                
+            case .failed, .cancelled:
+                print("P F or C")
+            case .possible:
+                print("PPossible")
+            }
+        }
+    }
+    
+    @objc func handleRotate(rotate: UIRotationGestureRecognizer){
+        switch rotate.state {
+        case .began:
+            self.gravity.removeItem(rotate.view!)
+        case .changed:
+            rotate.view?.transform = view.transform.rotated(by: rotate.rotation)
+            animator.updateItem(usingCurrentState: rotate.view!)
+        case .ended:
+            self.gravity.addItem(rotate.view!)
+        case .failed, .cancelled:
+            print("RF or C")
+        default:
+            break
+        }
+    }
+    
+    @objc func handleLongPress(press: UILongPressGestureRecognizer) {
+        switch press.state {
+        case .ended:
+            print("ended")
+            press.view?.backgroundColor = generateRandomColor()
+        case .failed, .cancelled:
+            print("F or C")
+        default:
+            break
+        }
     }
     
     @objc func panGesture(gesture: UIPanGestureRecognizer) {
         switch gesture.state {
         case .began:
             print("Began")
+            self.gravity.removeItem(gesture.view!)
         case .changed:
             print("Change to \(gesture.location(in: view))")
+            gesture.view?.center = gesture.location(in: gesture.view?.superview)
+            animator.updateItem(usingCurrentState: gesture.view!)
         case .ended:
             print("Ended")
+            self.gravity.addItem(gesture.view!)
         case .failed, .cancelled:
             print("F or C")
         case .possible:
@@ -72,19 +162,6 @@ class ViewController: UIViewController {
 }
 
 
-func generateRandomView(gpoint: CGPoint) -> UIView {
-    let random = arc4random_uniform(2)
-    if random == 0 {
-        let square = UIView(frame: CGRect(x: gpoint.x - 50, y: gpoint.y - 50, width:100, height:100))
-        return square
-    } else {
-        let circle = UIView(frame: CGRect(x: gpoint.x - 50, y: gpoint.y - 50, width: 100.0, height: 100.0))
-        circle.layer.cornerRadius = 50
-        circle.clipsToBounds = true
-        return circle
-    }
-}
-
 func generateRandomColor() -> UIColor {
     let hue : CGFloat = CGFloat(arc4random() % 256) / 256
     let saturation : CGFloat = CGFloat(arc4random() % 128) / 256 + 0.5
@@ -92,4 +169,3 @@ func generateRandomColor() -> UIColor {
     
     return UIColor(hue: hue, saturation: saturation, brightness: brightness, alpha: 1)
 }
-
